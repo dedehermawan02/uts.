090c7a104cb86c697c026b8ecda647199b217dd7
dedehermawan02 <131735446+dedehermawan02@users.noreply.github.com>
2023-11-26 18:47:22 +0700

    Add files via upload

--- 2106163_dede.sql
+++ 2106163_dede.sql
@@ -1,11 +1,75 @@
- CREATE DATABASE 2106163_dede;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Nov 2023 pada 12.44
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.4.23
 
- CREATE TABLE peserta(
-     id INT NOT NULL AUTO_INCREMENT,
-     nim VARCHAR(20) NOT NULL,
-     nama VARCHAR(50) NOT NULL,
-     kelas VARCHAR(20) NOT NULL,
-     email VARCHAR(50) NOT NULL,
-     jurusan VARCHAR(50) NOT NULL,
-     PRIMARY KEY(id)
-) ;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `2106163_dede`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peserta`
+--
+
+CREATE TABLE `peserta` (
+  `id` int(11) NOT NULL,
+  `nim` varchar(20) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `kelas` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `peserta`
+--
+
+INSERT INTO `peserta` (`id`, `nim`, `nama`, `kelas`, `email`, `jurusan`) VALUES
+(1, '2106163', 'dede', 'D', 'dh1122002@gmail.com', 'informatika'),
+(2, '', '', '', '', ''),
+(3, '', '', '', '', ''),
+(4, '2106163', 'dede', 'D', 'dh1122002@gmail.com', 'informatika'),
+(5, '', '', '', '', ''),
+(6, '2106163', 'dede hermawan', 'D', 'dh18102002@gmail.com', 'informatika'),
+(7, '2106163', 'dede hermawan', 'E', 'dh1122002@gmail.com', 'informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `peserta`
+--
+ALTER TABLE `peserta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `peserta`
+--
+ALTER TABLE `peserta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
